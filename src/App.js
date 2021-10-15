f05f0cbfbc6cf0823c2f353cac67582e7d0217d1
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-15 18:30:30 -0400

    add initialArray and useEffect to set initial array at App mount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import ChildComponent from './components/ChildComponent';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+const initialArray = [{number: 1}, {number: 2}, {number: 3}, {number: 4}, {number: 5}];
 
 function App() {
 
   const [myArray, setMyArray] = useState([])
+
+  useEffect(() => setMyArray(initialArray), []);
 
   return (
     <>
