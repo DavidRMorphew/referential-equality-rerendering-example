6e664e5d0c326852e7a9276ff2ebd3144ed2e64a
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-15 18:31:23 -0400

    convert App to arrow function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ChildComponent from './components/ChildComponent';
 import { useState, useEffect } from 'react';
 const initialArray = [{number: 1}, {number: 2}, {number: 3}, {number: 4}, {number: 5}];
 
-function App() {
+const App = () => {
 
   const [myArray, setMyArray] = useState([])
 
