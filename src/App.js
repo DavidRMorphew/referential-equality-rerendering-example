551668ab8831caa55cef9aa603a6be3ce9b05a09
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-15 18:28:54 -0400

    add useState hook for myArray

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import ChildComponent from './components/ChildComponent'
-
+import ChildComponent from './components/ChildComponent';
+import { useState } from 'react';
 
 function App() {
+
+  const [myArray, setMyArray] = useState([])
+
   return (
     <>
       Hello!
